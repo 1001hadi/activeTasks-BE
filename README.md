19aa4dc085f0fedd27f695eeef721ce46548243a
hadi.jaf <hadi.jaf.1001@gmail.com>
2025-06-02 11:51:31 -0600

    user Schema created

--- README.md
+++ README.md
@@ -6,7 +6,18 @@
 
 ## Build Process =>
 
-- Start project and install the necessary packages
-  - mongoose, express, dotenv, jsonwebtoken,bcryptjs ,cors,multer
-  - Files structures: Create folders and files
-  - setup the server.mjs and import necessary file and middlewares
+1- Start project and install the necessary packages
+
+- mongoose, express, dotenv, jsonwebtoken,bcryptjs ,cors,multer
+- Files structures: Create folders and files
+- setup the server.mjs and import necessary file and middlewares
+
+2- Data base setup
+
+- create folder and file to setup Data base with Mongoose
+- call it in server.mjs
+
+3- Setup Schemas
+
+- create user schema
+- create task schema
---
+++ models/User.js
@@ -0,0 +1,52 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please provide your first name"],
+      trim: true,
+      minLength: [6, "Name can't be less than 6 characters long"],
+      maxLength: [30, "Name can't be more than 30 characters long"],
+    },
+    email: {
+      type: String,
+      required: [true, "Please provide email address"],
+      trim: true,
+      unique: true,
+      match: [
+        /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+        "Email address must be valid !",
+      ],
+      // to create index with mongoose
+      index: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minLength: [6, "Password must be 6 characters long"],
+      //   match: [
+      //     /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[^A-Za-z0-9]).{8,}$/,
+      //     "Password must contain 8 characters including: 1 upper case,1 number and 1 special characters(#,?,!) ",
+      //   ],
+    },
+    profileImage: {
+      type: String,
+      default: null,
+    },
+    // for role based access like admin
+    role: {
+      type: String,
+      enum: ["admin", "user"],
+      default: "number",
+    },
+    registrationDate: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+
+  { timestamps: true }
+);
+
+export default mongoose.model("User", UserSchema);
