0455ccbec011406e8e45dadefa82d437f63902a7
hadi.jaf <hadi.jaf.1001@gmail.com>
2025-06-02 10:29:02 -0600

    fulderStructures updated and necessary packeges installed

--- server.mjs
+++ server.mjs
@@ -1,16 +1,26 @@
-import express from "express"
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
 
+dotenv.config();
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT;
 
 // middleware
+app.use(
+  cors({
+    origin: process.env.BASE_URL || "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-type", "Authorization"],
+  })
+);
 app.use(express.json());
+
 // routes
 
 // errMiddleware
 
-// listener 
-app.listen(PORT, ()=>{
-    console.log(`Server Run on Port: ${PORT}`);
-    
-})
+// listener
+app.listen(PORT, () => {
+  console.log(`Server Run on Port: ${PORT}`);
+});
